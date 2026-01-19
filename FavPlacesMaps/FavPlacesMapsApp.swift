7bfc9e35bb4267af3afd15f4a9b69fda6b7656c6
Alan Nakagawa <alannkgw@hotmail.com>
2026-01-19 18:28:02 -0300

    Correccion

---
+++ FavPlacesMaps/FavPlacesMapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavPlacesMapsApp.swift
+//  FavPlacesMaps
+//
+//  Created by Eichan Nakagawa on 04/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct FavPlacesMapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FavPlacesView()
+        }
+    }
+}
---
+++ FavPlacesMapsTests/FavPlacesMapsTests.swift
@@ -0,0 +1,16 @@
+//
+//  FavPlacesMapsTests.swift
+//  FavPlacesMapsTests
+//
+//  Created by Eichan Nakagawa on 04/07/2025.
+//
+
+import Testing
+
+struct FavPlacesMapsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
